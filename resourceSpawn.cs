41b4924eb9c2bf80da50e1a689d4159b54956e34
Cody Kern <143668697+CancEo1@users.noreply.github.com>
2025-02-01 11:02:37 -0700

    Add files via upload

---
+++ resourceSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+public class ResourceSpawn : MonoBehaviour
+{
+    // [Header("SpawnSettings")]
+    // public gameObject diamond;
+    // public float spawnChance;
+    // public int maxDiamondsToSpawn = 5;
+
+    // [Header("Raycast settings")] // if needed
+
+
+    void Start()
+    {
+        // spawnDiamonds();
+    }
+
+    void Update()
+    {
+        // add if/else statement here to handle spawn timing might be better to use Fixed Update class
+    }
+
+    public void spawnDiamonds()
+    {
+        // if (diamondSpawned >= maxDiamondsToSpawn)
+        // {
+        //    return; // spawn no more than 5 diamonds at a time
+        // }
+    }
+}
